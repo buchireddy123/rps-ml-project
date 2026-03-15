2a4290f8e47544fa64073ea906537a182d6fd5b5
BUCHIREDDY <buchireddyparpati@gmail.com>
2026-03-15 17:16:09 +0530

    Add main.py to play RPS against multiple players

---
+++ main.py
@@ -0,0 +1,14 @@
+from RPS_game import play, quincy, abbey, kris, mrugesh
+from RPS import player
+
+print("VS Quincy")
+play(player, quincy, 1000)
+
+print("VS Abbey")
+play(player, abbey, 1000)
+
+print("VS Kris")
+play(player, kris, 1000)
+
+print("VS Mrugesh")
+play(player, mrugesh, 1000)
